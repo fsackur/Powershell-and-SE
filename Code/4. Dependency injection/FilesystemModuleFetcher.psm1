27c80e56495995c76762b8bb1b18bbb4cf142ba3
Freddie Sackur <gitkraken@dustyfox.uk>
2019-04-01 00:17:32 +0100

    4. Dependency injection

---
+++ Code/4. Dependency injection/FilesystemModuleFetcher.psm1
@@ -0,0 +1,42 @@
+using namespace System.Collections.Generic
+using namespace System.Management.Automation
+using namespace Microsoft.PowerShell.Commands
+using module .\ModuleFetcher.psm1
+
+class FilesystemModuleFetcher : ModuleFetcher
+{
+    FilesystemModuleFetcher ()
+    {
+        $this.ModulePath = $env:PSModulePath -split ';' -replace '\\?$'
+    }
+
+    FilesystemModuleFetcher ([string[]] $ModulePath)
+    {
+        $this.ModulePath = $ModulePath -split ';' -replace '\\?$'
+    }
+
+    hidden [string] $ModulePath
+
+    [PSModuleInfo] GetModule([ModuleSpecification]$ModuleSpec)
+    {
+        $SearchPath = $this.ModulePath.ForEach({
+            Join-Path $_ $ModuleSpec.Name
+        })
+        Write-Debug "Searching '$($SearchPath -join "', '")'..."
+
+        $Module = (Get-Module $SearchPath -ListAvailable).Where(
+            {$true},
+            'First'
+        )
+
+        if ($Module)
+        {
+            Write-Debug "Found module '$Module'."
+            return $Module[0]
+        }
+        else
+        {
+            throw ([ItemNotFoundException]::new("No modules were found matching argument '$ModuleSpec'."))
+        }
+    }
+}
